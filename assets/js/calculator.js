699dd08175992685751c4cfbda9b6bd561db23fb
Soravith Puvekit <90691274+PinkMontyPie@users.noreply.github.com>
2024-06-12 13:15:21 +0700

    update fix and design theme

--- assets/js/calculator.js
+++ assets/js/calculator.js
@@ -16,20 +16,20 @@ function calculate(){
   let result = [];
   switch(type){
     case 'Common Phrases':
-      if(a.a!=null && a.b!=null) result.push(`${a.a*a.b/100} is ${a.a}% of ${a.b}`);
-      if(a.c!=null && a.d!=null) result.push(`${a.c} is ${a.c/a.d*100}% of ${a.d}`);
-      if(a.e!=null && a.f!=null) result.push(`${a.e} is ${a.f}% of ${a.e/a.f*100}`);
+      if(a.a!=null && a.b!=null) result.push(`${a.a*a.b/100} คือ ${a.a}% ของ ${a.b}`);
+      if(a.c!=null && a.d!=null) result.push(`${a.c} คือ ${a.c/a.d*100}% ของ ${a.d}`);
+      if(a.e!=null && a.f!=null) result.push(`${a.e} คือ ${a.f}% ของ ${a.e/a.f*100}`);
     break; 
     case 'Simple Percentage':
-      result.push(`${b.a}% of ${b.b} = ${b.a*b.b/100}`);
+      result.push(`${b.a}% ของ ${b.b} = ${b.a*b.b/100}`);
     break; 
     case 'Percentage Change':
-      if(c.a!=null && c.b!=null) result.push(`${c.a} increase ${c.b}% = ${c.a*(1+c.b/100)}`);
-      if(c.c!=null && c.d!=null) result.push(`${c.c} decrease ${c.d}% = ${c.c*(1-c.d/100)}`);
+      if(c.a!=null && c.b!=null) result.push(`${c.a} เพิ่มขึ้น ${c.b}% = ${c.a*(1+c.b/100)}`);
+      if(c.c!=null && c.d!=null) result.push(`${c.c} ลดลง ${c.d}% = ${c.c*(1-c.d/100)}`);
     break; 
     case 'Percentage Difference': 
-      result.push(`Difference of ${d.a} and ${d.b} is ${math.abs(d.a-d.b)/(d.a+d.b)*200}%,`);
-      result.push(`and ${d.b} is a ${math.abs(d.a-d.b)/d.a*100}% increase of ${d.a}`);
+      result.push(`ความแตกต่างของ ${d.a} และ ${d.b} คือ ${math.abs(d.a-d.b)/(d.a+d.b)*200}%,`);
+      result.push(`และ ${d.b} คือ ${math.abs(d.a-d.b)/d.a*100}% เพิ่มขึ้นของ ${d.a}`);
     break;
   }
 
--- ci_percentage_calculator.php
+++ ci_percentage_calculator.php
@@ -16,7 +16,7 @@ if (!function_exists('add_shortcode')) return "No direct call for Percentage Cal
 
 function display_calcio_ci_percentage_calculator(){
     $page = 'index.html';
-    return '<h2><img src="' . esc_url(plugins_url('assets/images/icon-48.png', __FILE__ )) . '" width="48" height="48">Percentage Calculator</h2><div><iframe style="background:transparent; overflow: scroll" src="' . esc_url(plugins_url($page, __FILE__ )) . '" width="100%" frameBorder="0" allowtransparency="true" onload="this.style.height = this.contentWindow.document.documentElement.scrollHeight + \'px\';" id="ci_percentage_calculator_iframe"></iframe></div>';
+    return '<h2>Percentage Calculator</h2><div><iframe style="background:transparent; overflow: scroll" src="' . esc_url(plugins_url($page, __FILE__ )) . '" width="100%" frameBorder="0" allowtransparency="true" onload="this.style.height = this.contentWindow.document.documentElement.scrollHeight + \'px\';" id="ci_percentage_calculator_iframe"></iframe></div>';
 }
 
 
